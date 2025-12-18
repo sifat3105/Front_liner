843cc34fd752716c78365a8184341a50bc144c2b
mdminhaj6565 <mdminhaj.dev00@gmail.com>
2025-12-18 18:58:45 +0600

    courier intgrat

---
+++ apps/courier/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import (
+    PaperflyRegistrationAPIView,
+    PaperflyOrderCreateAPIView,
+    PaperflyOrderTrackingAPIView,
+    PaperflyOrderCancelAPIView
+)
+
+urlpatterns = [
+    # Merchant Registration API
+    path('paperfly/register/', PaperflyRegistrationAPIView.as_view(), name='paperfly-register'),
+
+    # Order Create API
+    path('paperfly/order/create/', PaperflyOrderCreateAPIView.as_view(), name='paperfly-order-create'),
+
+    # Order Tracking API
+    path('paperfly/order/track/', PaperflyOrderTrackingAPIView.as_view(), name='paperfly-order-track'),
+
+    # order Cancellation API
+    path("paperfly/order/cancel/", PaperflyOrderCancelAPIView.as_view(), name="paperfly-order-cancel"),
+]
+
