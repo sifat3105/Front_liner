d2ab59202b97598f698677e4522bd27e683cc0b6
mdminhaj6565 <mdminhaj.dev00@gmail.com>
2026-01-01 13:42:12 +0600

    update account

--- apps/vendor/urls.py
+++ apps/vendor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import VendorRegistrationAPIView
+from .views import VendorRegistrationAPIView,VendorListAPIView
 
 urlpatterns = [
     path('vendors/register/',VendorRegistrationAPIView.as_view(),name='vendor-register'),
+    path('list/vendors/', VendorListAPIView.as_view(), name='vendor-list'),
 ]
--- apps/vendor/views.py
+++ apps/vendor/views.py
@@ -1,10 +1,11 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-
+from utils.base_view import BaseAPIView as APIView
+from .models import Vendor
 from .serializers import VendorSerializer
 
 
-class VendorRegistrationAPIView(BaseAPIView):
+class VendorRegistrationAPIView(APIView):
 
     permission_classes = [IsAuthenticated]
 
@@ -25,4 +26,25 @@ class VendorRegistrationAPIView(BaseAPIView):
             message="Vendor registration failed",
             errors=serializer.errors,
             status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class VendorListAPIView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        vendors = Vendor.objects.filter(
+            owner=request.user
+        ).order_by('-created_at')
+
+        serializer = VendorSerializer(vendors, many=True)
+
+        return self.success(
+            message="Vendor list fetched successfully",
+            data=serializer.data,
+            status_code=status.HTTP_200_OK,
+            meta={
+                "total_vendors": vendors.count()
+            }
         )
