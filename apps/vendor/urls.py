5302a3fc85d0dd85bd8275502edaa91113db0d94
mdminhaj6565 <mdminhaj.dev00@gmail.com>
2026-01-03 16:06:00 +0600

    a

--- apps/vendor/urls.py
+++ apps/vendor/urls.py
@@ -4,7 +4,10 @@ from .views import VendorRegistrationAPIView,VendorListAPIView,VendorPaymentHist
 urlpatterns = [
     path('vendors/register/',VendorRegistrationAPIView.as_view(),name='vendor-register'),
     path('list/vendors/', VendorListAPIView.as_view(), name='vendor-list'),
+
+
     path('update/vendors/<int:vendor_id>/', VendorDetailUpdateAPIView.as_view(), name='update-vendor'),
+    
     
     path('vendors/payments/history/', VendorPaymentHistoryAPIView.as_view(),name='payments-history'),
     path('vendors/payments/due/', VendorDueListAPIView.as_view(),name='payments-due'),
